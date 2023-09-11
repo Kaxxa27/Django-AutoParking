25b6e7ecd62f13f41b4c47f6c3ecc51d09883f70
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-09-11 13:56:53 +0300

    Added FAQ and vocabulary.

--- myparking/urls.py
+++ myparking/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
      path(r'', views.index, name='index'),
      path(r'about/', TemplateView.as_view(template_name="about.html"), name='about'),
+     path(r'faq/', TemplateView.as_view(template_name="faq.html"), name='FAQ'),
      path('register/', views.registration_view, name='register'),
 
      path('parking_list/', views.parking_list, name='parking_list'),
